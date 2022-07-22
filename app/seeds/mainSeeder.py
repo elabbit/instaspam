fc9663fb1187e0a8bc9351b01b0f3e1aec0c89b0
Eddie <Edwinjlau26@gmail.com>
2022-07-22 00:11:35 -0600

    SEEDERS DONE

--- app/seeds/mainSeeder.py
+++ app/seeds/mainSeeder.py
@@ -48,56 +48,61 @@ def seed_all():
         ownerId=2,
         image='https://www.spam.com/wp-content/uploads/2019/09/image-product_spam-classic-12oz-420x420.png',
         caption='The original. The timeless. The spiced hammiest of all SPAM® varieties. This is the taste that started it all back in 1937.',
-        post_likes=[]
     )
     post2 = Post(
         ownerId=2,
         image='https://www.spam.com/wp-content/uploads/2019/08/image-product_spam-with-real-hormel-bacon-12oz-420x420.png',
         caption="We know you're always in the mood for delicious SPAM® products, but what happens when you're also craving bacon? Fear not meat eater, for we have taken the best of both meats and canned it for your consumption!",
-        post_likes=[]
     )
     post3 = Post(
         ownerId=2,
         image='https://www.spam.com/wp-content/uploads/2019/08/image-product_spam-oven-roasted-turkey-12oz-420x420.png',
         caption="This SPAM® variety is made from 100% white, lean turkey. Some folks wonder why we didn't call it SPURKEY.",
-        post_likes=[]
     )
     post4 = Post(
         ownerId=2,
         image='https://www.spam.com/wp-content/uploads/2019/09/image-product_spam-hickory-smoke-12oz-420x420.png',
         caption='If the best thing to come from burning sticks is the bonfire, this is a close second.',
-        post_likes=[]
     )
     post5 = Post(
         ownerId=2,
         image='https://www.spam.com/wp-content/uploads/2019/08/image-product_spam-hot-and-spicy-12oz-420x420.png',
         caption='Ever wonder what pork would taste like if pigs subsisted on hot peppers?',
-        post_likes=[]
     )
     post6 = Post(
         ownerId=2,
         image='https://www.spam.com/wp-content/uploads/2019/09/image-product_spam-jalapeno-12oz-420x420.png',
         caption='Treat your taste buds to a fiesta of flavor with lively SPAM® Jalapeño.',
-        post_likes=[]
     )
     post7 = Post(
         ownerId=2,
         image='https://www.spam.com/wp-content/uploads/2019/09/image-product_spam-teriyaki-12oz-420x420.png',
         caption='To Musubi, or not to musubi, that should not be a question.',
-        post_likes=[]
     )
     post8 = Post(
         ownerId=2,
         image='https://www.spam.com/wp-content/uploads/2019/09/image-product_spam-portuguese-sausage-12oz-420x420.png',
         caption='"P.S." Have you ever wondered what that stands for at the end of a letter?',
-        post_likes=[]
     )
     post9 = Post(
         ownerId=2,
         image='https://www.spam.com/wp-content/uploads/2019/09/image-product_spam-tocino-12oz-420x420.png',
         caption='As a token of our love for this country, we have created a Filipino Flavor — SPAM® Tocino! It has just the right mixture of sweet and meaty to satisfy your craving for Filipino cuisine.',
-        post_likes=[]
     )
+
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.add(post3)
+    db.session.add(post4)
+    db.session.add(post5)
+    db.session.add(post6)
+    db.session.add(post7)
+    db.session.add(post8)
+    db.session.add(post9)
+    db.session.commit()
+
+
+
     post10 = Post(
         ownerId=3,
         image='https://www.worldatlas.com/r/w1200/upload/11/6f/5b/shutterstock-245388445-3-min.jpg',
@@ -120,7 +125,7 @@ def seed_all():
         ownerId=3,
         image='https://www.afarmgirlsdabbles.com/wp-content/uploads/2020/03/easy-hawaiian-poke-bowl_AFarmgirlsDabbles_AFD-7-sq-735x735.jpg',
         caption='Always in the mood for poki',
-        post_likes=[]
+        post_likes=[user5]
     )
     post14 = Post(
         ownerId=3,
@@ -191,7 +196,8 @@ def seed_all():
     post25 = Post(
         ownerId=4,
         image='https://pbs.twimg.com/media/FXF2bWsX0AADQd8.jpg',
-        caption='Lakes at the Great Smoky Mountains'
+        caption='Lakes at the Great Smoky Mountains',
+        post_likes=[user3]
     )
     post26 = Post(
         ownerId=4,
@@ -209,106 +215,115 @@ def seed_all():
         ownerId=5,
         image='https://vetstreet.brightspotcdn.com/dims4/default/bcde26b/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fd9%2F5b08a09ea111e0a2380050568d634f%2Ffile%2FAmerican-Cocker-2-645mk062111.jpg',
         caption='Groomed and Fluffy',
-        post_likes=[demo, user3, user4, user5, user6]
+        post_likes=[demo, user3, user4, user6]
     )
     post29 = Post(
         ownerId=5,
         image='https://www.k9rl.com/wp-content/uploads/2020/04/American-Cocker-Spaniel-Photo.jpg',
         caption="Straight hair don't care",
-        post_likes=[user5]
+        post_likes=[user3]
     )
     post30 = Post(
         ownerId=5,
         image='https://fidoseofreality.com/wp-content/uploads/2022/03/feature-photo-min.jpg',
         caption='Mini cocker spaniels',
-        post_likes=[user4, user5]
+        post_likes=[user4, user6]
     )
     post31 = Post(
         ownerId=5,
         image='https://www.espree.com/sites/default/files/2019-10/CockerSpaniel%28American%29.png',
-        caption='Pose.'
+        caption='Pose.',
+        post_likes=[user3, user4, user6]
     )
     post32 = Post(
         ownerId=5,
         image='https://www.vetdermclinic.com/wp-content/uploads/2019/05/VetDERM-Cocker-Spaniel-Care-Tips-Part-2-Breed-Specific-Dog-Skin-Problems.jpg',
-        caption='Been playing too much, need to rest'
+        caption='Been playing too much, need to rest',
+        post_likes=[demo, user3, user4, user6]
     )
     post33 = Post(
         ownerId=5,
         image='https://st.depositphotos.com/1806346/2417/i/950/depositphotos_24173463-stock-photo-american-cocker-spaniel-puppy-with.jpg',
-        caption='Me as a baby'
+        caption='Me as a baby',
+        post_likes=[user4]
     )
     post34 = Post(
         ownerId=5,
         image='https://www.dogbreedslist.info/uploads/dog-pictures/cocker-spaniel-2.jpg',
-        caption="It only looks like I'm sad, im actually pretty happy"
+        caption="It only looks like I'm sad, im actually pretty happy",
+        post_likes=[user3]
     )
     post35 = Post(
         ownerId=5,
         image='https://dogfoodsmart.com/wp-content/uploads/2021/04/American_Cocker_Spaniel_Growth_Chart.jpg',
-        caption='Striking another pose, I should compete.'
+        caption='Striking another pose, I should compete.',
+        post_likes=[demo, user6]
     )
     post36 = Post(
         ownerId=5,
         image='https://www.nbcsports.com/sites/nbcsports.com/files/2019/11/25/nbc_dogshow_sporting_cockerspanielparticolor_191123.jpg',
-        caption='On my way to win this thing.'
+        caption='On my way to win this thing.',
+        post_likes=[user3, user4]
     )
     post37 = Post(
         ownerId=6,
         image='https://wallpaperaccess.com/full/5044017.jpg',
         caption='I live my life a quarter mile at a time.',
+        post_likes=[user3, user4, user5]
     )
     post38 = Post(
         ownerId=6,
         image='https://wallpapercave.com/wp/wp3788220.jpg',
         caption='The mountains are calling and I must go'
+        ,
+        post_likes=[demo, user3, user4, user5]
     )
     post39 = Post(
         ownerId=6,
         image='https://wallpaperaccess.com/full/327437.jpg',
-        caption='A cloudy day is no match for a sunny disposition.'
+        caption='A cloudy day is no match for a sunny disposition.',
+        post_likes=[user3, user4]
     )
     post40 = Post(
         ownerId=6,
         image='https://wallpapercave.com/wp/wp3788129.jpg',
-        caption='Going to the mountains is like going home.'
+        caption='Going to the mountains is like going home.',
+        post_likes=[demo, user3, user4, user5]
     )
     post41 = Post(
         ownerId=6,
         image='https://wallpapercave.com/w/wp3788128',
-        caption='New York is a sucked orange.'
+        caption='New York is a sucked orange.',
+        post_likes=[user4]
     )
     post42 = Post(
         ownerId=6,
         image='https://www.lifewire.com/thmb/xlnqVmg658v1eQUqy1gbh3i5oNU=/1080x1080/smart/filters:no_upscale()/tropical-beach-wallpaper-beach-backgrounds-587fbb765f9b584db3241860.jpg',
-        caption="Life is a beach, I'm just playing in the sand."
+        caption="Life is a beach, I'm just playing in the sand.",
+        post_likes=[user3, user4, user5]
     )
     post43 = Post(
         ownerId=6,
         image='https://i2.wp.com/gramotoring.com/wp-content/uploads/2020/07/gramotoring-1595395337268.jpg?fit=1080%2C1080&ssl=1',
-        caption='Too soon junior.'
+        caption='Too soon junior.',
+        post_likes=[demo, user5]
     )
     post44 = Post(
         ownerId=6,
         image='https://cdn.shopify.com/s/files/1/2059/8303/files/Vendredi_13_octobre_-_Capita_Phil_Jacques.JPG?v=1583958107',
-        caption="You can't buy happiness, but you can buy a lift pass."
+        caption="You can't buy happiness, but you can buy a lift pass.",
+        post_likes=[user3]
     )
     post45 = Post(
         ownerId=6,
         image='https://i.pinimg.com/originals/42/3e/78/423e78b888c60c45869c106208c5aebf.jpg',
-        caption='The best climber in the world is the one having the most fun!'
+        caption='The best climber in the world is the one having the most fun!',
+        post_likes=[demo, user4, user5]
     )
 
 
-    db.session.add(post1)
-    db.session.add(post2)
-    db.session.add(post3)
-    db.session.add(post4)
-    db.session.add(post5)
-    db.session.add(post6)
-    db.session.add(post7)
-    db.session.add(post8)
-    db.session.add(post9)
+
+
     db.session.add(post10)
     db.session.add(post11)
     db.session.add(post12)
