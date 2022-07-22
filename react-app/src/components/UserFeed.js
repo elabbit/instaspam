4f894b052137136a9efd1d65ca6104e7c39cee17
David Chung <dvchung00@gmail.com>
2022-07-22 17:07:31 +0000

    UserFeed base setup

---
+++ react-app/src/components/UserFeed.js
@@ -0,0 +1,13 @@
+import { useDispatch } from "react-redux"
+
+
+
+const UserFeed = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <h1>user feed!!!!!</h1>
+  )
+}
+
+export default UserFeed;
