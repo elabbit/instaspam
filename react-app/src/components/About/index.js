ceb11c004849afa2239c60116905d47d154500c6
Jonathan <kim.jonathan426@gmail.com>
2022-07-25 16:26:21 -0400

    about page finished without descriptions and photos

--- react-app/src/components/About/index.js
+++ react-app/src/components/About/index.js
@@ -7,49 +7,14 @@ const About = () => {
             <div className='page-spacer' />
             <div className='page-container'>
                 <div> Welcome to instaspam... About us... Blurb here, Flask,</div>
+                <div>Meet our team...</div>
                 <div className='about-container'>
                     <div className='jade'>
                         <div className='about-user'>
                             <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-profile-image' />
                             <div className='about-header'>
                                 <div className='about-name'>jonathankim</div>
-                                <div className='about-subtext'>Kim.Jonathan426@gmail.com</div>
-                            </div>
-                        </div>
-                        <div className='about-image-container'>
-                            <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-image' />
-                        </div>
-                        <div className='about-comments'>
-                            <div className='about-comment'>
-                                <span className='about-name'>jonathankim&nbsp;</span>
-                                <span className='about-subtext'>Insert description and links here in this "caption"</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='jade'>
-                        <div className='about-user'>
-                            <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-profile-image' />
-                            <div className='about-header'>
-                                <div className='about-name'>abeltesfa</div>
-                                <div className='about-subtext'>Kim.Jonathan426@gmail.com</div>
-                            </div>
-                        </div>
-                        <div className='about-image-container'>
-                            <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-image' />
-                        </div>
-                        <div className='about-comments'>
-                            <div className='about-comment'>
-                                <span className='about-name'>davidchung&nbsp;</span>
-                                <span className='about-subtext'>Insert description and links here in this "caption"</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='jade'>
-                        <div className='about-user'>
-                            <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-profile-image' />
-                            <div className='about-header'>
-                                <div className='about-name'>eddielau</div>
-                                <div className='about-subtext'>Kim.Jonathan426@gmail.com</div>
+                                <div className='about-subtext'>kim.jonathan426@gmail.com</div>
                             </div>
                         </div>
                         <div className='about-image-container'>
@@ -61,19 +26,18 @@ const About = () => {
                                     jonathankim&nbsp;
                                     <span className='about-subtext'>
                                         Description of anything you want to write about yourself
-                                        <div>Github: <a href="https://github.com/elabbit"> <i className="fa-brands fa-github"></i></a></div>
-                                        <div>LinkedIn: <a href="https://www.linkedin.com/in/edwin-lau-312a11241/"> <i className="fa-brands fa-linkedin-in"></i></a></div>
+                                        <div>Github: <a href="https://github.com/KimJonathan426" target='_blank'> <i className="fa-brands fa-github"></i></a></div>
+                                        <div>LinkedIn: <a href="https://www.linkedin.com/in/kimjonathan426" target='_blank'> <i className="fa-brands fa-linkedin-in"></i></a></div>
                                     </span>
                                 </span>
                             </div>
                         </div>
-                    </div>
-                    <div className='jade'>
+                    </div><div className='jade'>
                         <div className='about-user'>
                             <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-profile-image' />
                             <div className='about-header'>
-                                <div className='about-name'>jonathankim</div>
-                                <div className='about-subtext'>Kim.Jonathan426@gmail.com</div>
+                                <div className='about-name'>abeltesfa</div>
+                                <div className='about-subtext'>abeltesfa@gmail.com</div>
                             </div>
                         </div>
                         <div className='about-image-container'>
@@ -81,8 +45,60 @@ const About = () => {
                         </div>
                         <div className='about-comments'>
                             <div className='about-comment'>
-                                <span className='about-name'>jonathankim&nbsp;</span>
-                                <span className='about-subtext'>Insert description and links here in this "caption"</span>
+                                <span className='about-name'>
+                                    abeltesfa&nbsp;
+                                    <span className='about-subtext'>
+                                        Description of anything you want to write about yourself
+                                        <div>Github: <a href="https://github.com/abeltesfa" target='_blank'> <i className="fa-brands fa-github"></i></a></div>
+                                        <div>LinkedIn: <a href="https://www.linkedin.com/in/abel-tesfa-93121866/" target='_blank'> <i className="fa-brands fa-linkedin-in"></i></a></div>
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div><div className='jade'>
+                        <div className='about-user'>
+                            <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-profile-image' />
+                            <div className='about-header'>
+                                <div className='about-name'>davidchung</div>
+                                <div className='about-subtext'>dvchung00@gmail.com</div>
+                            </div>
+                        </div>
+                        <div className='about-image-container'>
+                            <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-image' />
+                        </div>
+                        <div className='about-comments'>
+                            <div className='about-comment'>
+                                <span className='about-name'>
+                                    davidchung&nbsp;
+                                    <span className='about-subtext'>
+                                        Description of anything you want to write about yourself
+                                        <div>Github: <a href="https://github.com/dchung007" target='_blank'> <i className="fa-brands fa-github"></i></a></div>
+                                        <div>LinkedIn: <a href="https://www.linkedin.com/in/david-chung-98a5651aa/" target='_blank'> <i className="fa-brands fa-linkedin-in"></i></a></div>
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div><div className='jade'>
+                        <div className='about-user'>
+                            <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-profile-image' />
+                            <div className='about-header'>
+                                <div className='about-name'>eddielau</div>
+                                <div className='about-subtext'>edwinjlau26@gmail.com</div>
+                            </div>
+                        </div>
+                        <div className='about-image-container'>
+                            <img src='https://instaspambucket.s3.us-west-1.amazonaws.com/user5-LuckyProfile.png' className='about-image' />
+                        </div>
+                        <div className='about-comments'>
+                            <div className='about-comment'>
+                                <span className='about-name'>
+                                    eddielau&nbsp;
+                                    <span className='about-subtext'>
+                                        Description of anything you want to write about yourself
+                                        <div>Github: <a href="https://github.com/elabbit" target='_blank'> <i className="fa-brands fa-github"></i></a></div>
+                                        <div>LinkedIn: <a href="https://www.linkedin.com/in/edwin-lau-312a11241/" target='_blank'> <i className="fa-brands fa-linkedin-in"></i></a></div>
+                                    </span>
+                                </span>
                             </div>
                         </div>
                     </div>
