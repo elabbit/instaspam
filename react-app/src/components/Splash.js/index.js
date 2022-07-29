9a66942382205db7e53825fa74a0bdaa1378bd6c
Abel Tesfa <abeltesfa@gmail.com>
2022-07-29 19:35:20 -0400

    not there yet

--- react-app/src/components/Splash.js/index.js
+++ react-app/src/components/Splash.js/index.js
@@ -4,7 +4,12 @@ import { useHistory } from 'react-router-dom';
 import { login } from '../../store/session';
 import LoginForm from '../auth/LoginForm';
 import './Splash.css'
-import splashImg from '../../images/instaspamsplash.png'
+import splashImg1 from '../../images/instaspamsplashimg1.png'
+import splashImg2 from '../../images/instaspamsplashimg2.png'
+import splashImg3 from '../../images/instaspamsplashimg3.png'
+import realsplashImg1 from '../../images/instaspamsplashrealimg1.png'
+import realsplashImg2 from '../../images/instaspamsplashrealimg2.png'
+import realsplashImg3 from '../../images/instaspamsplashrealimg3.png'
 import SignUpForm from '../auth/SignUpForm';
 import instaspamLogo from "../../images/instaspam-logo.png"
 
@@ -15,7 +20,11 @@ function Splash({ sessionUser }) {
   const email = 'demo@email.com';
   const password = 'password';
 
+  const splashImages = [splashImg1, splashImg2, splashImg3];
+  // const splashImages = [realsplashImg1, realsplashImg2, realsplashImg3];
+
   const [displayLogin, setDisplayLogin] = useState(true);
+  const [imgIndex, setImgIndex] = useState(0);
 
   const demoLogin = async (e) => {
     await dispatch(login(email, password))
@@ -27,13 +36,25 @@ function Splash({ sessionUser }) {
     }
   }, [sessionUser, history])
 
+  useEffect(()=> {
+    const imgId = setInterval(()=> {
+      if(imgIndex === splashImages.length -1) {
+        setImgIndex(0);
+      }
+      else {
+        setImgIndex(imgIndex + 1)
+      }
+    }, 7000)
+    return () => clearInterval(imgId);
+  }, [splashImages.length, imgIndex])
+
 
   return (
     <div className="page-outer">
     <div className='page-splash-container'>
       <div className='page-splash-content'>
         <div className='page-splash-content-left'>
-          <img src={splashImg} alt="" />
+          <img className='page-splash-img' src={splashImages[imgIndex]} alt="" />
         </div>
         <div className='page-splash-content-right'>
           <div className='page-splash-content-form'>
