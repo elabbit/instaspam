e8e4ae6cd359d227665f214cdbf7adc50b35392e
Eddie <Edwinjlau26@gmail.com>
2022-07-29 17:36:04 -0600

    DONE CROP

--- react-app/src/components/CreatePostModal/CreatePost.js
+++ react-app/src/components/CreatePostModal/CreatePost.js
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { addPost } from '../../store/posts'
 import CropEasy from "../Crop/CropEasy";
 import ErrorModal from '../ErrorModal';
+import { ReactComponent as EmojiBox } from '../../images/emoji-box.svg';
+import Picker from 'emoji-picker-react';
+import { ReactComponent as Photo } from '../../images/photo.svg';
 
 const CreatePostNew = ({ hideModal }) => {
     const dispatch = useDispatch();
@@ -16,6 +19,43 @@ const CreatePostNew = ({ hideModal }) => {
     const [openUpload, setOpenUpload] = useState(true)
     const [openCrop, setOpenCrop] = useState(false)
     const [openCreate, setOpenCreate] = useState(false)
+    const [emojiBox, setEmojiBox] = useState(false);
+    const [eventListener, setEventListener] = useState(false);
+
+    const handleDocumentClick = (event) => {
+        let isEmojiClassFound = false;
+        event &&
+        event.composedPath() &&
+        event.composedPath().forEach(elem => {
+          if (elem && elem.classList) {
+            const data = elem.classList.value;
+            if (data.includes("emoji")) {
+              isEmojiClassFound = true;
+            }
+          }
+        });
+        if (isEmojiClassFound === false && event.target.id !== "emojis-btn") {
+          setEmojiBox(false);
+          setEventListener(false);
+          document.removeEventListener("click", handleDocumentClick);
+        }
+      };
+
+    const onEmojiClick = (event, emojiObject) => {
+        setCaption((caption) => caption + emojiObject.emoji);
+        setEmojiBox(!emojiBox);
+      };
+
+      const showEmojiBox = (e) => {
+        e.preventDefault();
+
+        if (emojiBox === false && !eventListener) {
+          document.addEventListener("click", handleDocumentClick, false);
+          setEventListener(true)
+        }
+
+        setEmojiBox(!emojiBox);
+      }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -80,7 +120,8 @@ const CreatePostNew = ({ hideModal }) => {
                 <div className="upload-crop-create">
                     {openUpload && (
                         <div className="create-post-image-upload-field">
-                            {/* <label className="image-label" for="create-post-files">Upload image:</label> */}
+                            <Photo/>
+                            <label className="create-image-label" for="create-post-files">Select from computer</label>
                             <input
                                 className="create-post-image-upload-section"
                                 id="create-post-files"
@@ -102,17 +143,29 @@ const CreatePostNew = ({ hideModal }) => {
                                 <img src={photoURL} alt='' />
                             </div>
                             <div className="create-right">
-                                <div>
-                                    <img></img>
-                                    <div className="create-username"></div>
+                                <div className="create-post-profile-pic-username">
+                                    <img className="create-post-profile-pic" src={sessionUser.profileImage} alt="" />
+                                    <div className="create-post-profile-username">
+                                        {sessionUser.username}
+                                    </div>
                                 </div>
+                                <div className="create-txt-emoji">
                                 <textarea
                                     className="create-post-form-caption"
                                     value={caption}
                                     onChange={(e) => setCaption(e.target.value)}
                                     placeholder='Write a caption...'
                                     maxLength="1000"
-                                />
+                                    />
+                                <button id='emojis-btn' onClick={showEmojiBox} className='create-show-emojis'><EmojiBox /></button>
+                                {emojiBox && (
+                                    <>
+                                        <div className='create-emoji-form'>
+                                            <Picker onEmojiClick={onEmojiClick} />
+                                        </div>
+                                    </>
+                                )}
+                                </div>
                             </div>
                         </div>
                     )}
