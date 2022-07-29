bad4117d2f6853f923b2886dd5183311c21c4938
David Chung <dvchung00@gmail.com>
2022-07-29 23:09:54 +0000

    Working on search bar react component

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -1,23 +1,71 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Select} from "react-select";
+// import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+// import { getAllUsers } from "../../store/user";
 
 
 const SearchBar = () => {
-  const dispatch = useDispatch();
-  const posts = useSelector(state => state.posts);
-  const [postsData, setPostsData] = useState([false]);
+  // const dispatch = useDispatch();
+  // const users = useSelector(state => state.user.users);
+  const [users, setUsers] = useState('');
+  const [usersData, setUsersData] = useState('');
+  const [dropdown, setDropdown] = useState(false);
+  const [searchInput, setSearchInput] = useState('')
 
-  useEffect(()=> {
-    const fetch
-  } )
 
+  const fetchUsers = () => {
+    fetch('/api/users/all')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, []);
+
+  // console.log(users)
+
+  const handleChange = (searchWord) => {
+    // console.log(searchWord)
+
+    if (searchWord) {
+      const searchResults = Object.values(users).filter(user => user.username.startsWith(searchWord))
+      setUsersData(searchResults);
+      setDropdown(true);
+      // setSearchInput(searchWord)
+    } else {
+      setDropdown(false)
+    }
+  }
+
+  const clickResult = () => {
+    setDropdown(false);
+    setSearchInput('');
+  }
+  // console.log(users)
 
   return (
-    <form>
-      <input placeholder="Search"></input>
-      <button>Submit</button>
-    </form>
+    <div>
+      <input
+        placeholder="Search..."
+        onChange={(e) => handleChange(e.target.value)}
+        value={searchInput}
+      >
+      </input>
+      <div>
+        {usersData && dropdown ?
+          usersData.map(user => (
+            <div key={user.id}>
+              <Link onClick={() => clickResult} to={`/${user.username}`}>
+                {user.username}
+              </Link>
+            </div>
+          ))
+          :
+          null
+        }
+      </div>
+    </div>
   );
 }
 
--- react-app/src/store/user.js
+++ react-app/src/store/user.js
@@ -1,23 +1,39 @@
 import { addFollow, removeFollow } from "./session";
 
 const GET_USER = 'session/GET_USER'
+const GET_ALL_USERS = 'session/GET_ALL_USERS'
 
 export const getUserAction = (user) => ({
-    type: GET_USER,
-    payload: user
+  type: GET_USER,
+  payload: user
+})
+
+export const getAllUsersAction = (users) => ({
+  type: GET_ALL_USERS,
+  payload: users
 })
 
 export const getUser = (username) => async (dispatch) => {
-    const response = await fetch(`/api/users/${username}`);
+  const response = await fetch(`/api/users/${username}`);
 
-    if(response.ok) {
-        const user = await response.json();
-        dispatch(getUserAction(user))
-    }
-
+  if (response.ok) {
+    const user = await response.json();
+    dispatch(getUserAction(user))
   }
 
-export const follow = (userId) => async(dispatch) => {
+}
+
+// export const getAllUsers = () => async (dispatch) => {
+//   const response = await fetch('/api/users/all')
+
+//   if (response.ok) {
+//     const users = await response.json();
+//     dispatch(getAllUsersAction(users));
+//     return users;
+//   }
+// }
+
+export const follow = (userId) => async (dispatch) => {
   const response = await fetch(`/api/follows/${userId}`, {
     method: 'PUT',
     headers: {
@@ -25,15 +41,15 @@ export const follow = (userId) => async(dispatch) => {
     }
   });
 
-    if(response.ok) {
-      const user = await response.json();
-      // dispatch(getUserAction(user))
-      dispatch(addFollow(user))
-      return user
-    }
+  if (response.ok) {
+    const user = await response.json();
+    // dispatch(getUserAction(user))
+    dispatch(addFollow(user))
+    return user
+  }
 }
 
-export const unfollow = (userId) => async(dispatch) => {
+export const unfollow = (userId) => async (dispatch) => {
   const response = await fetch(`/api/follows/unfollow/${userId}`, {
     method: 'PUT',
     headers: {
@@ -41,20 +57,27 @@ export const unfollow = (userId) => async(dispatch) => {
     }
   });
 
-    if(response.ok) {
-      const user = await response.json();
-      // dispatch(getUserAction(user))
-      dispatch(removeFollow(user))
-      return user
-    }
+  if (response.ok) {
+    const user = await response.json();
+    // dispatch(getUserAction(user))
+    dispatch(removeFollow(user))
+    return user
+  }
 }
 
 
-  export default function reducer(state = {}, action) {
-    switch (action.type) {
-      case GET_USER:
-        return { [action.payload.username]: action.payload }
-      default:
-        return state;
-    }
+export default function reducer(state = {}, action) {
+  switch (action.type) {
+    case GET_USER:
+      return { [action.payload.username]: action.payload }
+    // case GET_ALL_USERS:
+    //   const newState1 = {};
+    //   newState1.users = {};
+    //   action.payload.users.forEach(user => {
+    //     newState1.users[user.username] = user
+    //   })
+    //   return newState1;
+    default:
+      return state;
+  }
 }
