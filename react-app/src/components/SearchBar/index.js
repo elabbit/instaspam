975b97b36ef55957fccadbb5cda881de99e267df
David Chung <dvchung00@gmail.com>
2022-07-29 15:39:41 +0000

    first steps

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Select} from "react-select";
 
 
 const SearchBar = () => {
   const dispatch = useDispatch();
   const posts = useSelector(state => state.posts);
   const [postsData, setPostsData] = useState([false]);
+
+  useEffect(()=> {
+    const fetch
+  } )
+
 
   return (
     <form>
