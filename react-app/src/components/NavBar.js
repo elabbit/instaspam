8f37e59f7ac760f94747cf471a75fd0ba9a5acf7
David Chung <dvchung00@gmail.com>
2022-07-30 23:52:13 +0000

    Search bar styling

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -11,6 +11,7 @@ import instaspamLogo from "../images/instaspam-logo.png"
 import about from "../images/about.png"
 import aboutfill from "../images/about-fill.png"
 import Dropdown from './Dropdown';
+import SearchBar from './SearchBar';
 
 
 const NavBar = () => {
@@ -54,6 +55,9 @@ const NavBar = () => {
               <NavLink to='/' exact={true} activeClassName='active'>
                 <img className="navbar-logo" src={instaspamLogo} alt=""></img>
               </NavLink>
+            </div>
+            <div className='nav-middle'>
+              <SearchBar />
             </div>
             <div className="nav-right">
               <div className="home-icon-div">
---
+++ react-app/src/components/SearchBar/index.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Search } from "../../images/search.svg";
+import "./SearchBar.css"
+
+const SearchBar = () => {
+  const [users, setUsers] = useState('');
+  const [usersData, setUsersData] = useState('');
+  const [dropdown, setDropdown] = useState(false);
+  const [searchInput, setSearchInput] = useState('')
+
+  const fetchUsers = () => {
+    fetch('/api/users/')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, []);
+
+  const handleChange = (searchWord) => {
+    if (searchWord) {
+      const searchResults = users.users.filter(user => user.username.startsWith(searchWord))
+
+      setUsersData(searchResults);
+      setDropdown(true);
+      setSearchInput(searchWord)
+    } else {
+      setSearchInput(searchWord)
+      setDropdown(false)
+    }
+  }
+
+  const clickResult = () => {
+    setDropdown(false);
+    setSearchInput('');
+  }
+
+  return (
+    <div className="search-bar">
+      <div className="search-field">
+        <Search className="search-icon" />
+        <input
+          placeholder="Search"
+          onChange={(e) => handleChange(e.target.value)}
+          value={searchInput}
+        >
+        </input>
+      </div>
+      <div className={dropdown ? "search-results" : "search-none"}>
+        {dropdown &&
+          usersData.map(user => (
+            <div key={user.id} className="search-user">
+              <Link onClick={() => clickResult()} to={`/${user.username}`} >
+                <div>
+                  <img src={user.profileImage} />
+                </div>
+                <div className="search-user-info">
+                  <div className="search-username">
+                    {user.username}
+                  </div>
+                  <div className="search-user-name">
+                    {user.name}
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))
+        }
+      </div>
+    </div >
+  )
+}
+
+export default SearchBar;
