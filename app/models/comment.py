cf286a7128322408eb8ca07bd71726dda268f48d
Eddie <Edwinjlau26@gmail.com>
2022-07-31 17:30:39 -0600

    done

--- app/models/comment.py
+++ app/models/comment.py
@@ -9,7 +9,7 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     postId = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
-    comment = db.Column(db.String(1000), nullable=True)
+    comment = db.Column(db.String(100), nullable=True)
     createdAt = db.Column(DateTime(timezone=True), server_default=func.now())
 
     user_id = db.relationship("User", back_populates="user_comments")
